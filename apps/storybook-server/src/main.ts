5678ea521e41982641c02e662c15110d9d293c90
Dave Harms <daveharmswebdev@gmail.com>
2022-02-25 10:24:55 -0600

    serving petstore

---
+++ apps/storybook-server/src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+import * as path from 'path';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/petstore/?*', (req, res) => {
+  // res.send({ message: 'Welcome to storybook-server!' });
+  res.sendFile(path.join(__dirname, '..', 'petstore', 'index.html'));
+});
+
+const port = process.env.port || 8080;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/`);
+});
+server.on('error', console.error);
